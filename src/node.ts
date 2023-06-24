80b43eae939f825ad7a0a2895b6c1803fd338b38
Tom Busillo <1016306+tbusillo@users.noreply.github.com>
2023-06-24 08:59:42 -0400

    fix: remove node scope from crypto import (#31)
    ---------
    
    Co-authored-by: Tom <tbusillo@users.noreply.github.com>

--- src/node.ts
+++ src/node.ts
@@ -7,7 +7,7 @@ import { formatUUID, setSignificantBits } from './utils'
  */
 
 export const node = (): string => {
-  const crypto = require('node:crypto').webcrypto
+  const crypto = require('crypto').webcrypto
   // Generate 16 random bytes
   const bytes: Uint8Array = crypto.getRandomValues(new Uint8Array(16))
 
